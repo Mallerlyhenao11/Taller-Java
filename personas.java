af807a70f668e8c3cb07fdc69aac72ea84c37701
Mallerly <mallerlyvanesahenaogallego@gmail.com>
2024-05-04 17:35:57 -0500

    Encapsulamiento - Implementación de Métodos para Manipulación de Propiedades por Mallerly Henao

--- personas.java
+++ personas.java
@@ -1,10 +1,38 @@
 import java.util.*;
 
-public class personas {
+public class Personas {
 
     private int edad;
     private String nombre;
     private Date fechaNacimiento;
 
-    
+    //Metodo constructor 
+    //es el que le da la intancia al objeto 
+
+    public Personas (int edad, String nombre, Date fechaNacimiento){
+        this.edad = edad;
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        // El this es el que le da la instrancia a los atributoss         
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+
+    }
 }
